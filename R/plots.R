7438618daabda9f2c55ca314ccf611e391f492da
Shawn Gilroy <shawn.gilroy@temple.edu>
2017-08-04 10:01:53 -0400

    Move remaining figures

--- R/plots.R
+++ R/plots.R
@@ -9,7 +9,7 @@
 #' @param lineWidth Line width
 #' @author Shawn Gilroy <shawn.gilroy@temple.edu>
 #' @importFrom reshape melt
-#' @importFrom ggplot2 geom_point geom_line aes annotation_logticks element_blank element_line element_rect element_text expand_limits guide_legend guides labs scale_x_continuous theme theme_bw xlab ylab
+#' @importFrom ggplot2 geom_point geom_line aes annotation_logticks element_blank element_line element_rect element_text expand_limits guide_legend guides labs scale_x_continuous scale_colour_manual theme theme_bw xlab ylab
 #' @importFrom scales trans_breaks trans_format math_format
 #' @return display figure
 displayED50Figure <- function(dat, results, lineWidth = 1) {
@@ -237,4 +237,479 @@ displayED50Figure <- function(dat, results, lineWidth = 1) {
     guides(col = guide_legend(ncol = 3))
 
   print(logChart)
+}
+
+#' Display most probable fitted series with AUC metric
+#'
+#' This method constructs a figure that displays the most probable model
+#' as well as the probability that it is the "true" model.  The
+#' Model-based AUC metric is also provided for the most probable model.
+#'
+#' @param dat observed data
+#' @param results Results of analyses for data series
+#' @param lineWidth Line width
+#' @author Shawn Gilroy <shawn.gilroy@temple.edu>
+#' @return display figure
+displayAUCFigure <- function(dat, results, lineWidth = 1) {
+
+  samuelsonK <- NA
+  ainslieK <- NA
+  betaConstant <- NA
+  deltaConstant <- NA
+  myerK <- NA
+  myerS <- NA
+  rachK <- NA
+  rachS <- NA
+  epK <- NA
+  epS <- NA
+  crdiK <- NA
+  crdiS <- NA
+  crdiBeta <- NA
+  ghK <- NA
+  ghBeta <- NA
+
+  endDelay <- max(dat$X)
+  delaySeries = 1:(endDelay+1)
+  expSeries  = rep(NA,endDelay+1)
+  hypSeries  = rep(NA,endDelay+1)
+  quaSeries  = rep(NA,endDelay+1)
+  myerSeries = rep(NA,endDelay+1)
+  rachSeries = rep(NA,endDelay+1)
+  epSeries = rep(NA,endDelay+1)
+  crdiSeries = rep(NA,endDelay+1)
+  ghSeries = rep(NA,endDelay+1)
+
+  legend = c("Empirical: ")
+  colors = c("black", "black")
+
+  if ("Exponential.lnk" %in% names(results)) {
+    samuelsonK <- results[["Exponential.lnk"]]
+  }
+
+  if ("Hyperbolic.lnk" %in% names(results)) {
+    ainslieK <- results[["Hyperbolic.lnk"]]
+  }
+
+  if ("Laibson.beta" %in% names(results)) {
+    betaConstant <- results[["Laibson.beta"]]
+    deltaConstant <- results[["Laibson.delta"]]
+  }
+
+  if ("GreenMyerson.lnk" %in% names(results)) {
+    myerK <- results[["GreenMyerson.lnk"]]
+    myerS <- results[["GreenMyerson.s"]]
+  }
+
+  if ("Rachlin.lnk" %in% names(results)) {
+    rachK <- results[["Rachlin.lnk"]]
+    rachS <- results[["Rachlin.s"]]
+  }
+
+  if ("EbertPrelec.lnk" %in% names(results)) {
+    epK <- results[["EbertPrelec.lnk"]]
+    epS <- results[["EbertPrelec.s"]]
+  }
+
+  if ("BleichrodtCRDI.lnk" %in% names(results)) {
+    crdiK <- results[["BleichrodtCRDI.lnk"]]
+    crdiS <- results[["BleichrodtCRDI.s"]]
+    crdiBeta <- results[["BleichrodtCRDI.beta"]]
+  }
+
+  if ("RodriguezLogue.lnk" %in% names(results)) {
+    ghK <- results[["RodriguezLogue.lnk"]]
+    ghBeta <- results[["RodriguezLogue.beta"]]
+  }
+
+  for (delay in delaySeries)
+  {
+    delaySeries[delay] = delay-1
+
+    if(!is.na(samuelsonK))
+    {
+      expSeries[delay] = (1.0 * exp(-(exp(samuelsonK))*delay))
+    }
+
+    if(!is.na(ainslieK))
+    {
+      hypSeries[delay] = 1.0 * (1+exp(ainslieK)*delay)^(-1)
+    }
+
+    if(!is.na(betaConstant))
+    {
+      quaSeries[delay] = 1.0 * ((betaConstant)*(deltaConstant)^delay)
+    }
+
+    if(!is.na(myerK))
+    {
+      myerSeries[delay] = 1.0 * (1+exp(myerK)*delay)^(-myerS)
+    }
+
+    if(!is.na(rachK))
+    {
+      rachSeries[delay] = 1.0 * (1 + exp(rachK)*(delay^rachS))^(-1)
+    }
+
+    if(!is.na(epK))
+    {
+      epSeries[delay] = 1.0 * exp(-(exp(epK)*delay)^epS)
+    }
+
+    if(!is.na(crdiK))
+    {
+      crdiSeries[delay] = 1.0 * crdiBeta * exp(-exp(crdiK)*delay^crdiS)
+    }
+
+    if(!is.na(ghK))
+    {
+      ghSeries[delay] = 1.0 * (1 + delay * exp(ghK))^(-ghBeta / exp(ghK))
+    }
+  }
+
+  mData <- data.frame(X = dat$X, Y = dat$Y)
+
+  lineColor <- "black"
+
+  if (results[["probable.model"]] == "Hyperbolic") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = hypSeries)
+    legend = c(legend, paste("Hyperbolic(",
+                             round(results[["Hyperbolic.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Exponential") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = expSeries)
+    legend = c(legend, paste("Exponential(",
+                             round(results[["Exponential.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Laibson") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = quaSeries)
+    legend = c(legend, paste("Laibson(",
+                             round(results[["Laibson.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "GreenMyerson") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = myerSeries)
+    legend = c(legend, paste("GreenMyerson(",
+                             round(results[["GreenMyerson.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Rachlin") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = rachSeries)
+    legend = c(legend, paste("Rachlin(",
+                             round(results[["Rachlin.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "EbertPrelec") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = epSeries)
+    legend = c(legend, paste("EbertPrelec(",
+                             round(results[["EbertPrelec.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "BleichrodtCRDI") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = crdiSeries)
+    legend = c(legend, paste("BleichrodtCRDI(",
+                             round(results[["BleichrodtCRDI.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "RodriguezLogue") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = ghSeries)
+    legend = c(legend, paste("RodriguezLogue(",
+                             round(results[["RodriguezLogue.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else {
+    totalFrame = data.frame(Delays = delaySeries)
+    totalFrame$ModelArea <- results[["Noise.mean"]]
+
+    legend = c(legend, paste("Noise(",
+                             round(results[["Noise.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  }
+
+  graphics::plot(totalFrame$Delays, totalFrame$ModelArea, type = "l", ylim = c(0,1),
+                 main = paste("Participant ", dat$id[1],
+                              "\n", results[["probable.model"]], " Model Area = ", round(results[["probable.AUC"]], 5),
+                              sep = ""),
+                 xlab = "Delays",
+                 ylab = "Value",
+                 col = lineColor,
+                 lwd = lineWidth)
+
+  graphics::points(dat$X,
+                   dat$Y,
+                   type = "p",
+                   cex = 2,
+                   pch = 18)
+
+  graphics::lines(dat$X,
+                  dat$Y,
+                  col = "black",
+                  lty = 2,
+                  lwd = 0.5)
+
+  legend("topright",
+         legend = legend,
+         col = colors,
+         lty = c(2, 1),
+         lwd = 3,
+         title = "Model (Probability)")
+}
+
+#' Display most probable fitted series with AUC metric (log10)
+#'
+#' This method constructs a figure that displays the most probable model
+#' as well as the probability that it is the "true" model.  The
+#' Model-based AUC metric in log base 10 scale is also provided
+#' for the most probable model.
+#'
+#' @param dat observed data
+#' @param results Results of analyses for data series
+#' @param lineWidth Line width
+#' @author Shawn Gilroy <shawn.gilroy@temple.edu>
+#' @return display figure
+displayLogAUCFigure <- function(dat, results, lineWidth = 1) {
+
+  samuelsonK <- NA
+  ainslieK <- NA
+  betaConstant <- NA
+  deltaConstant <- NA
+  myerK <- NA
+  myerS <- NA
+  rachK <- NA
+  rachS <- NA
+  epK <- NA
+  epS <- NA
+  crdiK <- NA
+  crdiS <- NA
+  crdiBeta <- NA
+  ghK <- NA
+  ghBeta <- NA
+
+  endDelay <- max(dat$X)
+  delaySeries = 1:(endDelay+1)
+  expSeries  = rep(NA,endDelay+1)
+  hypSeries  = rep(NA,endDelay+1)
+  quaSeries  = rep(NA,endDelay+1)
+  myerSeries = rep(NA,endDelay+1)
+  rachSeries = rep(NA,endDelay+1)
+  epSeries = rep(NA,endDelay+1)
+  crdiSeries = rep(NA,endDelay+1)
+  ghSeries = rep(NA,endDelay+1)
+
+  legend = c(paste("Noise: ", round(results[["Noise.prob"]], 5), sep = ""))
+  colors = c("red")
+
+  legend = c("Empirical:")
+  colors = c("black", "black")
+
+  if ("Exponential.lnk" %in% names(results)) {
+    samuelsonK <- results[["Exponential.lnk"]]
+  }
+
+  if ("Hyperbolic.lnk" %in% names(results)) {
+    ainslieK <- results[["Hyperbolic.lnk"]]
+  }
+
+  if ("Laibson.beta" %in% names(results)) {
+    betaConstant <- results[["Laibson.beta"]]
+    deltaConstant <- results[["Laibson.delta"]]
+  }
+
+  if ("GreenMyerson.lnk" %in% names(results)) {
+    myerK <- results[["GreenMyerson.lnk"]]
+    myerS <- results[["GreenMyerson.s"]]
+  }
+
+  if ("Rachlin.lnk" %in% names(results)) {
+    rachK <- results[["Rachlin.lnk"]]
+    rachS <- results[["Rachlin.s"]]
+  }
+
+  if ("EbertPrelec.lnk" %in% names(results)) {
+    epK <- results[["EbertPrelec.lnk"]]
+    epS <- results[["EbertPrelec.s"]]
+  }
+
+  if ("BleichrodtCRDI.lnk" %in% names(results)) {
+    crdiK <- results[["BleichrodtCRDI.lnk"]]
+    crdiS <- results[["BleichrodtCRDI.s"]]
+    crdiBeta <- results[["BleichrodtCRDI.beta"]]
+  }
+
+  if ("RodriguezLogue.lnk" %in% names(results)) {
+    ghK <- results[["RodriguezLogue.lnk"]]
+    ghBeta <- results[["RodriguezLogue.beta"]]
+  }
+
+  for (delay in delaySeries)
+  {
+    delaySeries[delay] = delay-1
+
+    if(!is.na(samuelsonK))
+    {
+      expSeries[delay] = (1.0 * exp(-(exp(samuelsonK))*delay))
+    }
+
+    if(!is.na(ainslieK))
+    {
+      hypSeries[delay] = 1.0 * (1+exp(ainslieK)*delay)^(-1)
+    }
+
+    if(!is.na(betaConstant))
+    {
+      quaSeries[delay] = 1.0 * ((betaConstant)*(deltaConstant)^delay)
+    }
+
+    if(!is.na(myerK))
+    {
+      myerSeries[delay] = 1.0 * (1+exp(myerK)*delay)^(-myerS)
+    }
+
+    if(!is.na(rachK))
+    {
+      rachSeries[delay] = 1.0 * (1 + exp(rachK)*(delay^rachS))^(-1)
+    }
+
+    if(!is.na(epK))
+    {
+      epSeries[delay] = 1.0 * exp(-(exp(epK)*delay)^epS)
+    }
+
+    if(!is.na(crdiK))
+    {
+      crdiSeries[delay] = 1.0 * crdiBeta * exp(-exp(crdiK)*delay^crdiS)
+    }
+
+    if(!is.na(ghK))
+    {
+      ghSeries[delay] = 1.0 * (1 + delay * exp(ghK))^(-ghBeta / exp(ghK))
+    }
+  }
+
+  mData <- data.frame(X = dat$X, Y = dat$Y)
+
+  lineColor <- "black"
+
+  if (results[["probable.model"]] == "Hyperbolic") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = hypSeries)
+    legend = c(legend, paste("Hyperbolic(",
+                             round(results[["Hyperbolic.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Exponential") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = expSeries)
+    legend = c(legend, paste("Exponential(",
+                             round(results[["Exponential.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Laibson") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = quaSeries)
+    legend = c(legend, paste("Laibson(",
+                             round(results[["Laibson.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "GreenMyerson") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = myerSeries)
+    legend = c(legend, paste("GreenMyerson(",
+                             round(results[["GreenMyerson.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "Rachlin") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = rachSeries)
+    legend = c(legend, paste("Rachlin(",
+                             round(results[["Rachlin.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "EbertPrelec") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = epSeries)
+    legend = c(legend, paste("EbertPrelec(",
+                             round(results[["EbertPrelec.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "BleichrodtCRDI") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = crdiSeries)
+    legend = c(legend, paste("BleichrodtCRDI(",
+                             round(results[["BleichrodtCRDI.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else if (results[["probable.model"]] == "RodriguezLogue") {
+    totalFrame = data.frame(Delays = delaySeries,
+                            ModelArea = ghSeries)
+    legend = c(legend, paste("RodriguezLogue(",
+                             round(results[["RodriguezLogue.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  } else {
+    totalFrame = data.frame(Delays = delaySeries)
+    totalFrame$ModelArea <- results[["Noise.mean"]]
+
+    legend = c(legend, paste("Noise(",
+                             round(results[["Noise.prob"]], 5),
+                             ")",
+                             sep = ""))
+
+  }
+
+  graphics::plot(totalFrame$Delays, totalFrame$ModelArea, type = "l", ylim = c(0,1),
+                 main = paste("Participant ", dat$id[1],
+                              "\n", results[["probable.model"]], " Scaled Model Area = ", round(results[["probable.Log10AUC"]], 5),
+                              sep = ""),
+                 xlab = "Delays",
+                 ylab = "Value",
+                 col = lineColor,
+                 lwd = lineWidth,
+                 log = "x")
+
+  graphics::points(dat$X,
+                   dat$Y,
+                   type = "p",
+                   cex = 2,
+                   pch = 18)
+
+  graphics::lines(dat$X,
+                  dat$Y,
+                  col = "black",
+                  lty = 2,
+                  lwd = 0.5)
+
+  legend("topright",
+         legend = legend,
+         col = colors,
+         lty = c(2, 1),
+         lwd = 3,
+         title = "Model (Probability)")
 }
