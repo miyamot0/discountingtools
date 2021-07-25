99f6ca922074bc4e8297e3845c917f9e4cf027c6
Shawn Gilroy <shawnpgilroy@gmail.com>
2021-07-25 12:41:03 -0500

    Add in group plot option

---
+++ demo/testGroup.R
@@ -0,0 +1,58 @@
+rm(list = ls())
+
+library(dplyr)
+library(discountingtools)
+library(tidyr)
+
+set.seed(65535)
+
+dataFrame = data.frame(
+  ids = 1:10,
+  ks  = NA
+)
+
+dataFrame$ks  = rnorm(length(dataFrame$ids), 0.09, 0.05)
+dataFrame$ks  = log(dataFrame$ks)
+
+delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
+
+for (row in 1:nrow(dataFrame)) {
+  ys = hyperbolicDiscountFunc(delays, dataFrame[row, "ks"]) + rnorm(length(delays),
+                                                                    0,
+                                                                    0.05)
+
+  dataFrame[row, as.character(delays)] = ys
+}
+
+dataFrame[,"1"]    = ifelse(dataFrame[,"1"] > 1,    1, dataFrame[,"1"])
+dataFrame[,"8640"] = ifelse(dataFrame[,"8640"] < 0, 0, dataFrame[,"8640"])
+
+dataFrame.long = dataFrame %>%
+  gather(Delay, Value, -ids, -ks) %>%
+  mutate(Delay = as.numeric(Delay))
+
+str(dataFrame.long)
+
+results = fitDDCurves(data = dataFrame.long,
+            settings = list(Delays     = Delay,
+                            Values     = Value,
+                            Individual = ids),
+            maxValue = 1) %>%
+  dd_modelOptions(plan = c("mazur",
+                           "bleichrodt",
+                           "ebertprelec",
+                           "exponential",
+                           "greenmyerson",
+                           "laibson",
+                           "noise",
+                           "rachlin",
+                           "rodriguezlogue")) %>%
+  dd_metricOptions(metrics = c("lned50",
+                               "mbauc",
+                               "logmbauc")) %>%
+  dd_analyze()
+
+# TODO rodrig mbauc
+# TODO warn types
+
+plot(results, logAxis = "x", position = "topright")
