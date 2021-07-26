b0028500e57599d104b6dfc75352d3de097fad08
Shawn Gilroy <shawnpgilroy@gmail.com>
2021-07-26 11:00:49 -0500

    Increase size of single model demo

--- demo/testSingle.R
+++ demo/testSingle.R
@@ -7,11 +7,11 @@ library(tidyr)
 set.seed(65535)
 
 dataFrame = data.frame(
-  ids = 1:30,
+  ids = 1:100,
   ks  = NA
 )
 
-dataFrame$ks  = rnorm(length(dataFrame$ids), 0.125, 0.05)
+dataFrame$ks  = rnorm(length(dataFrame$ids), 0.35, 0.125)
 dataFrame$ks  = log(dataFrame$ks)
 
 delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
@@ -19,24 +19,25 @@ delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
 for (row in 1:nrow(dataFrame)) {
   ys = hyperbolicDiscountFunc(delays, dataFrame[row, "ks"]) + rnorm(length(delays),
                                                                     0,
-                                                                    0.05)
+                                                                    0.025)
 
   dataFrame[row, as.character(delays)] = ys
 }
 
 dataFrame.long = dataFrame %>%
   gather(Delay, Value, -ids, -ks) %>%
-  mutate(Delay = as.numeric(Delay))
-
-dataFrame.long[,"Value"] = ifelse(dataFrame.long[,"Value"] > 1, 1, dataFrame.long[,"Value"])
-dataFrame.long[,"Value"] = ifelse(dataFrame.long[,"Value"] < 0, 0, dataFrame.long[,"Value"])
+  mutate(Delay = as.numeric(Delay)) %>%
+  mutate(Value = ifelse(Value < 0, 0, Value)) %>%
+  mutate(Value = ifelse(Value > 1, 0, Value))
 
 results = fitDDCurves(data = dataFrame.long,
             settings = list(Delays     = Delay,
                             Values     = Value,
                             Individual = ids),
-            maxValue = 1) %>%
-  dd_modelOptions(plan = c("mazur")) %>%
+            maxValue = 1,
+            verbose  = TRUE) %>%
+  dd_modelOptions(plan   = c("mazur")) %>%
+  dd_screenOption(screen = FALSE) %>%
   dd_analyze(modelSelection = FALSE)
 
 summary(results)
