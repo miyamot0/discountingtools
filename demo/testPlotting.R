b09267f228adfd7e3ae6f1bfd5e78303cd3c61be
Shawn Gilroy <shawnpgilroy@gmail.com>
2021-07-25 21:52:58 -0500

    Add test plot code

---
+++ demo/testPlotting.R
@@ -0,0 +1,82 @@
+rm(list = ls())
+
+library(dplyr)
+library(discountingtools)
+library(tidyr)
+
+set.seed(65535)
+
+dataFrame = data.frame(
+  ids = 1:30,
+  ks  = NA,
+  grp = "A"
+)
+
+dataFrame$ks  = rnorm(length(dataFrame$ids), 0.125, 0.05)
+dataFrame$ks  = log(dataFrame$ks)
+
+delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
+
+for (row in 1:nrow(dataFrame)) {
+  ys = hyperbolicDiscountFunc(delays, dataFrame[row, "ks"]) + rnorm(length(delays),
+                                                                    0,
+                                                                    0.05)
+
+  dataFrame[row, as.character(delays)] = ys
+}
+
+dataFrame2 = data.frame(
+  ids = 31:60,
+  ks  = NA,
+  grp = "B"
+)
+
+dataFrame2$ks  = rnorm(length(dataFrame2$ids), 0.225, 0.025)
+dataFrame2$ks  = log(dataFrame2$ks)
+
+for (row in 1:nrow(dataFrame2)) {
+  ys = hyperbolicDiscountFunc(delays, dataFrame2[row, "ks"]) + rnorm(length(delays),
+                                                                    0,
+                                                                    0.05)
+
+  dataFrame2[row, as.character(delays)] = ys
+}
+
+dataFrame = rbind(dataFrame,
+                  dataFrame2)
+
+dataFrame.long = dataFrame %>%
+  gather(Delay, Value, -ids, -ks, -grp) %>%
+  mutate(Delay = as.numeric(Delay))
+
+dataFrame.long[,"Value"] = ifelse(dataFrame.long[,"Value"] > 1, 1, dataFrame.long[,"Value"])
+dataFrame.long[,"Value"] = ifelse(dataFrame.long[,"Value"] < 0, 0, dataFrame.long[,"Value"])
+
+results = fitDDCurves(data = dataFrame.long,
+            settings = list(Delays     = Delay,
+                            Values     = Value,
+                            Individual = ids,
+                            Group      = grp),
+            maxValue = 1) %>%
+  dd_modelOptions(plan = c("mazur",
+                           "bleichrodt",
+                           "ebertprelec",
+                           "exponential",
+                           "greenmyerson",
+                           "laibson",
+                           "noise",
+                           "rachlin",
+                           "rodriguezlogue")) %>%
+  dd_metricOptions(metrics = c("lned50",
+                              "mbauc",
+                              "logmbauc")) %>%
+  dd_analyze()
+
+summary(results)
+
+plot(results, which = "ED50")
+
+plot(results, which = "MBAUC")
+
+plot(results, which = "Log10MBAUC")
+
