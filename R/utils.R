c9082b7ab8dc36aec27e9fc18c7d6d248ed91c95
Shawn Gilroy <shawnpgilroy@gmail.com>
2021-07-27 11:46:29 -0500

    Add in individual plotting

--- R/utils.R
+++ R/utils.R
@@ -349,18 +349,20 @@ summary.discountingtools <- function(fittingObject, detailed = TRUE) {
 #' @param xlab0 (char) x axis label
 #' @param logAxis (char) axis designation
 #' @param yMin (num) y axis lower limit
+#' @param id (num) participant number to focus
 #'
 #' @return
 #' @author Shawn Gilroy <sgilroy1@lsu.edu>
 #' @export plot.discountingtools
 #' @export
-plot.discountingtools <- function(fittingObject, which = "ind", position0 = "bottomleft", ylab0 = "Subjective Value", xlab0 = "Delay", logAxis = "x", yMin = 0.01) {
+plot.discountingtools <- function(fittingObject, which = "ind", position0 = "bottomleft", ylab0 = "Subjective Value", xlab0 = "Delay", logAxis = "x", yMin = 0.01, id = NULL) {
 
-  if (which == "ind")        plotIndividualRainbow(fittingObject, position0, ylab0, xlab0, logAxis, yMin)
-  if (which == "group")      plotGroupRainbow(fittingObject,      position0, ylab0, xlab0, logAxis, yMin)
-  if (which == "ED50")       plotRainbowCross(fittingObject, metric = "LnED50")
-  if (which == "MBAUC")      plotRainbowCross(fittingObject, metric = "MBAUC")
-  if (which == "Log10MBAUC") plotRainbowCross(fittingObject, metric = "Log10MBAUC")
+  if (which == "ind" & is.null(id))        plotIndividualRainbow(fittingObject, position0, ylab0, xlab0, logAxis, yMin)
+  if (which == "ind" & !is.null(id))       plotIndividualDetailed(fittingObject, position0, ylab0, xlab0, logAxis, yMin, id)
+  if (which == "group")                    plotGroupRainbow(fittingObject,      position0, ylab0, xlab0, logAxis, yMin)
+  if (which == "ED50")                     plotRainbowCross(fittingObject, metric = "LnED50")
+  if (which == "MBAUC")                    plotRainbowCross(fittingObject, metric = "MBAUC")
+  if (which == "Log10MBAUC")               plotRainbowCross(fittingObject, metric = "Log10MBAUC")
 }
 
 #' plotIndividualRainbow
@@ -394,7 +396,7 @@ plotIndividualRainbow <- function(fittingObject, position0, ylab0, xlab0, logAxi
     # Hack: Check if even multiple models
 
     if (is.null(fittingObject$rotation)) {
-      model = names(results$results[[id]])
+      model = names(fittingObject$results[[id]])
     } else {
       model  = fittingObject$rotation[[id]]$ProbableModel
     }
@@ -458,6 +460,118 @@ plotIndividualRainbow <- function(fittingObject, position0, ylab0, xlab0, logAxi
          col    = legendBuildColor,
          lty    = 1)
 
+}
+
+#' plotIndividualDetailed
+#'
+#' This implementation of plot singles out a particular responder, providing the fits to the observed data as well as the probability that the "probable" model characterizes the data
+#'
+#' @param fittingObject core fitting object
+#' @param position0 (char) position of legend
+#' @param ylab0 (char) y axis label
+#' @param xlab0 (char) x axis label
+#' @param logAxis (char) axis designation
+#' @param yMin (num) y axis lower limit
+#' @param id (num) participant id
+#'
+#' @return
+#' @author Shawn Gilroy <sgilroy1@lsu.edu>
+plotIndividualDetailed <- function(fittingObject, position0, ylab0, xlab0, logAxis, yMin, id) {
+  if (!(id %in% names(fittingObject$results))) stop('id not found in results')
+
+  if (grepl("y", logAxis) == TRUE) {
+    yLimits = c(yMin, fittingObject$maxValue)
+  } else {
+    yLimits = c(0,    fittingObject$maxValue)
+  }
+
+  vecModels = fittingObject$models
+  vecColors = rainbow(length(vecModels), alpha = 1)
+
+  preBuiltLegend   = FALSE
+  legendBuildModel = NA
+  legendBuildColor = NA
+
+  ogData = subset(fittingObject$data, ids == id)
+
+  plot(ogData[,as.character(fittingObject$settings['Delays'])],
+       ogData[,as.character(fittingObject$settings['Values'])],
+       type = "p",
+       ylim = yLimits,
+       log  = logAxis,
+       main = "Summary Fits",
+       col  = "black",
+       pch  = 19,
+       ylab = ylab0,
+       xlab = xlab0)
+
+  for (model in names(fittingObject$results[[id]])) {
+    result = fittingObject$results[[id]][[model]]
+
+    xs = seq(min(ogData[,as.character(fittingObject$settings['Delays'])]),
+             max(ogData[,as.character(fittingObject$settings['Delays'])]), length.out = 2000)
+
+    if (model == "noise")          yhat = rep(result$Intercept, length(xs))
+
+    if (model == "bleichrodt")     yhat = BleichrodtCRDIDiscountFunc(xs,     result$Lnk,  result$S, result$Beta)
+    if (model == "ebertprelec")    yhat = ebertPrelecDiscountFunc(xs,        result$Lnk,  result$S)
+    if (model == "exponential")    yhat = exponentialDiscountFunc(xs,        result$Lnk)
+    if (model == "greenmyerson")   yhat = myersonHyperboloidDiscountFunc(xs, result$Lnk,  result$S)
+    if (model == "laibson")        yhat = betaDeltaDiscountFunc(xs,          result$Beta, result$Delta)
+    if (model == "mazur")          yhat = hyperbolicDiscountFunc(xs,         result$Lnk)
+    if (model == "rachlin")        yhat = rachlinHyperboloidDiscountFunc(xs, result$Lnk,  result$S)
+    if (model == "rodriguezlogue") yhat = RodriguezLogueDiscountFunc(xs,     result$Lnk,  result$Beta)
+
+    if (length(vecColors) == 1) {
+      col = vecColors
+    } else {
+      col = vecColors[match(model, vecModels)]
+    }
+
+    if (!(model %in% legendBuildModel)) {
+      probString = ""
+
+      modelP = gsub("ebertprelec",    "ebert prelec",    model)
+      modelP = gsub("greenmyerson",   "green myerson",   modelP)
+      modelP = gsub("rodriguezlogue", "rodriguez logue", modelP)
+
+      modelC = tools::toTitleCase(modelP)
+      modelC = gsub(" ", "~", modelC)
+
+      if (grepl(model, fittingObject$rotation[[id]]$ProbableModel) == TRUE) {
+        probString = paste0("~(",
+                            as.character(round(fittingObject$rotation[[id]]$ProbableModel.Prob, 3)),
+                            ")")
+
+        modelP = parse(text = paste0("bold(",modelC, probString,")"))
+      } else {
+        modelP = parse(text = paste0(modelC, probString))
+      }
+
+      if (!preBuiltLegend) {
+        legendBuildModel = c(modelP)
+        legendBuildColor = c(col)
+
+        preBuiltLegend   = TRUE
+      } else {
+        legendBuildModel = c(legendBuildModel, modelP)
+        legendBuildColor = c(legendBuildColor, col)
+      }
+    }
+
+    if (grepl("y", logAxis) == TRUE) {
+      yhat    = yhat[yhat >= 0]
+    }
+
+    lines(xs, yhat * fittingObject$maxValue,
+          col  = col)
+  }
+
+  legend(position0,
+         legend = legendBuildModel,
+         col    = legendBuildColor,
+         lty    = 1,
+         bty    = "n")
 }
 
 #' plotGroupRainbow
