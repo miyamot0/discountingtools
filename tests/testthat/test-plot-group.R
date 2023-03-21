88d9b6f5b398c53ae107cc5b93136e5a6df689b7
Shawn Gilroy <shawnpgilroy@gmail.com>
2023-03-20 20:13:05 -0500

    Add in more tests, bump up coverage

---
+++ tests/testthat/test-plot-group.R
@@ -0,0 +1,92 @@
+
+describe("dd_plot: Various Individuals", {
+  library(tidyr)
+  library(dplyr)
+  library(discountingtools)
+
+  n_per_group <- 5
+
+  data_frame = data.frame(
+    ids = seq_len(n_per_group),
+    ks  = NA,
+    grp = "Group A"
+  )
+
+  data_frame$ks  = rnorm(length(data_frame$ids), 0.35, 0.125)
+  data_frame$ks  = log(data_frame$ks)
+
+  delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
+
+  data_frame$auc = dd_mbauc_mazur(1, data_frame$ks, min(delays), max(delays))
+
+  for (row in 1:nrow(data_frame)) {
+    ys = dd_discount_func_mazur(delays, data_frame[row, "ks"]) + rnorm(length(delays),
+                                                                       0,
+                                                                       0.05)
+
+    data_frame[row, as.character(delays)] = ys
+  }
+
+  data_frame2 = data.frame(
+    ids = 50 + seq_len(n_per_group),
+    ks  = NA,
+    grp = "Group B"
+  )
+
+  data_frame2$ks  = rnorm(length(data_frame2$ids), 0.075, 0.035)
+  data_frame2$ks  = log(data_frame2$ks)
+
+  data_frame2$auc = dd_mbauc_mazur(1, data_frame2$ks, min(delays), max(delays))
+
+  for (row in 1:nrow(data_frame2)) {
+    ys = dd_discount_func_mazur(delays, data_frame2[row, "ks"]) + rnorm(length(delays),
+                                                                        0,
+                                                                        0.025)
+
+    data_frame2[row, as.character(delays)] = ys
+  }
+
+  data_frame = rbind(data_frame,
+                     data_frame2)
+
+  data_frame_long = data_frame %>%
+    gather(Delay, Value, -ids, -ks, -grp, -auc) %>%
+    mutate(Delay = as.numeric(Delay)) %>%
+    mutate(Value = ifelse(Value < 0, 0, Value)) %>%
+    mutate(Value = ifelse(Value > 1, 0, Value))
+
+  results = fit_dd_curves(data = data_frame_long,
+                          settings = list(Delays     = Delay,
+                                          Values     = Value,
+                                          Individual = ids,
+                                          Group      = grp),
+                          plan = c("mazur", "exponential"),
+                          maxValue = 1,
+                          verbose  = FALSE) |>
+    dd_analyze(modelSelection = TRUE)
+
+  it("Plots individually in groups: Predictions", {
+    expect_no_error(
+      plot(results, logAxis = "x", position = "topright", which = "group")
+    )
+  })
+
+  it("Plots individually: ED50", {
+    expect_no_error(
+      plot(results, logAxis = "x", position = "topright", which = "ED50")
+    )
+  })
+
+  it("Plots individually: MBAUC", {
+    expect_no_error(
+      plot(results, logAxis = "x", position = "topright", which = "MBAUC")
+    )
+  })
+
+  it("Plots individually: MBAUC Log10 Scaled", {
+    expect_no_error(
+      plot(results, logAxis = "x", position = "topright", which = "Log10MBAUC")
+    )
+  })
+})
+
---
+++ tests/testthat/test-plot-ind.R
@@ -0,0 +1,66 @@
+
+describe("dd_plot: Various Individuals", {
+  library(tidyr)
+  library(dplyr)
+  library(discountingtools)
+
+  dataFrame = data.frame(
+    ids = 1:10,
+    ks  = NA
+  )
+
+  dataFrame$ks  = rnorm(length(dataFrame$ids), 0.07, 0.03)
+  dataFrame$ks  = log(dataFrame$ks)
+
+  delays = c(1, 30, 180, 540, 1080, 2160, 4320, 8640)
+
+  for (row in seq_len(nrow(dataFrame))) {
+    ys = dd_discount_func_mazur(delays, dataFrame[row, "ks"]) + rnorm(length(delays),
+                                                                      0,
+                                                                      0.025)
+
+    dataFrame[row, as.character(delays)] = ys
+  }
+
+  data_frame = dataFrame %>%
+    tidyr::gather(Delay, Value, -ids, -ks) %>%
+    dplyr::mutate(Delay = as.numeric(Delay)) %>%
+    dplyr::mutate(Value = ifelse(Value < 0, 0, Value)) %>%
+    dplyr::mutate(Value = ifelse(Value > 1, 0, Value))
+
+  results = fit_dd_curves(
+    data = data_frame,
+    settings = list(Delays     = Delay,
+                    Values     = Value,
+                    Individual = ids),
+    maxValue = 1,
+    plan = c('mazur')) |>
+    dd_analyze(modelSelection = TRUE)
+
+  it("Plots individually: Predictions", {
+    expect_no_error(
+      plot(results,
+           logAxis = "x",
+           position = "topright")
+    )
+  })
+
+  it("Plots individually: ED50", {
+    expect_no_error(
+      plot(results, which = "ED50")
+    )
+  })
+
+  it("Plots individually: MBAUC", {
+    expect_no_error(
+      plot(results, which = "MBAUC")
+    )
+  })
+
+  it("Plots individually: MBAUC Log10 Scaled", {
+    expect_no_error(
+      plot(results, which = "Log10MBAUC")
+    )
+  })
+})
+
